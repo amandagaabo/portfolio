80c5f250e3981c404e6275fc48ccc27f6e52248c
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-12-24 20:07:04 -0700

    added shrinking nav

--- index.js
+++ index.js
@@ -29,8 +29,19 @@ function startApp() {
     })
   };
 
+  function handleNavSize() {
+    $(document).on("scroll", function() {
+    	if($(document).scrollTop() > 100) {
+    		$(".topnav").removeClass("large-nav").addClass("small-nav");
+    	} else {
+    		$(".topnav").removeClass("small-nav").addClass("large-nav");
+    	}
+    });
+  };
+
   handleNavIconClick();
   handleNavLinkClick();
+  handleNavSize();
 
 };
 
