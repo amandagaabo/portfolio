38cfda1daa719e133fa1a7298cceda39a5ffdf49
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-12-24 19:12:03 -0700

    added nice scroll to section on menu click

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function startApp() {
-
   //toggle responsive class when mobil nav icon is clicked
   function handleNavIconClick() {
     $('#nav-icon').click(function() {
@@ -7,16 +6,30 @@ function startApp() {
     })
   };
 
-  handleNavIconClick();
-
   function handleNavLinkClick() {
     $('#myTopnav').on('click', '.nav-link', function() {
+      // get current nav
+      const currentNav = $(this);
+
+      // if mobile and drop down menu is shown then hide the top menu on click
       if ($('#myTopnav').hasClass('show-menu')) {
-        $('#myTopnav').removeClass("show-menu");
+        $('#myTopnav').removeClass('show-menu');
       }
+
+      // nice scroll to the path in the link
+      const path = $(this).attr('href');
+      $.scrollTo($(`${path}`), {
+        axis: 'y',
+        duration: 1000,
+        specialEasing: {
+          width: 'linear',
+          height: 'easeInOutSine'
+        }
+      });
     })
   };
 
+  handleNavIconClick();
   handleNavLinkClick();
 
 };
