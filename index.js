c39a0c8921b4024b6e2fda885d64bb094d8ad837
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-12-27 17:17:10 -0700

    added resume link, lint fixes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 function startApp() {
-
-  //toggle show-menu class when mobile hamburger nav icon is clicked
+  // toggle show-menu class when mobile hamburger nav icon is clicked
   function handleNavIconClick() {
-    $('#nav-icon').click(function() {
+    $('#nav-icon').click(() => {
       $('#myTopnav').toggleClass('show-menu');
     });
   }
 
   function handleNavLinkClick() {
-    $('#myTopnav').on('click', '.nav-link', function() {
-
+    $('#myTopnav').on('click', '.nav-link', () => {
       // if mobile and show-menu is active then hide the top menu on nave link click
       if ($('#myTopnav').hasClass('show-menu')) {
         $('#myTopnav').removeClass('show-menu');
@@ -30,7 +28,7 @@ function startApp() {
 
   // make the nav bar smaller when the user scrolls down
   function handleNavSize() {
-    $(document).on('scroll', function() {
+    $(document).on('scroll', () => {
       if ($(document).scrollTop() > 100) {
         $('.topnav').removeClass('large-nav').addClass('small-nav');
       } else {
